ddf350de6057479ffed0413e6d5b546e2ab21c4d
David Rowbotham <thedaver64@gmail.com>
2025-09-29 08:11:47 +0100

    removed more test stuff.

--- test_compile.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "nonexistent.h"
-
-int main() {
-    undeclared_variable = 5;
-    another_undefined_function();
-    printf("This will have errors\n");
-    return 0;
-}
--- test_settings.c
+++
@@ -1,7 +0,0 @@
-/* Test file for settings commands */
-#include <stdio.h>
-
-int main() {
-    printf("Hello, world!\n");
-    return 0;
-}
